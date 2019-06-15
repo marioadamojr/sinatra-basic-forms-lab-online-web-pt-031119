7fbff4a787557c33479f879b42547ed7361e4935
Mario Adamo Jr <marioadamojr@gmail.com>
2019-06-15 23:39:29 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,6 +1,6 @@
 class Puppy
-  attr_reader :name, :breed
-  attr_accessor :months_old
+  attr_reader :breed
+  attr_accessor :name, :months_old
 
   def initialize(name, breed, months_old)
     @name=name
