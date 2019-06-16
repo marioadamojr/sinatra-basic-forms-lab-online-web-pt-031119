55b83610ccb661431c2b4371171a606344b436a6
Mario Adamo Jr <marioadamojr@gmail.com>
2019-06-16 00:01:49 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,6 +1,6 @@
 class Puppy
-  attr_reader :breed
-  attr_accessor :name, :months_old
+  attr_reader 
+  attr_accessor :name, :breed, :months_old
 
   def initialize(hash_info)
     hash_info.each do |k,v|
